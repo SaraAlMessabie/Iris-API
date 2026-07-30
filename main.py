bb4c229212687cc19965862fa02dfae73af7bbe4
SaraAlMessabie <saraalmessabie@gmail.com>
2026-07-30 10:35:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    alow_methods=["*"],
-    allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
